ab650864876c757b52676c2f4a1f1b9350699f27
Lemuel <lemuel@lemuel>
2019-01-12 22:05:11 -0800

    add data to Owners controller

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnersIndex.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnersIndex.java
@@ -1,14 +1,24 @@
 package com.example.petclinic.controllers;
 
+import com.example.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("owners")
 @Controller
 public class OwnersIndex {
 
+    private OwnerService ownerService;
+
+    public OwnersIndex(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOfVets() {
+    public String listOfVets(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
